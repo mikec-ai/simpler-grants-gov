00881f86c5fc352ebf9044602ebe4ef0e7bb9787
mikec-ai <mike@mikec.ai>
2026-08-23 09:43:38 -0400

    Test portable R&R SF-424 lifecycle (#25)

--- api/src/constants/lookup_constants.py
+++ api/src/constants/lookup_constants.py
@@ -161,6 +161,7 @@ class FormType(StrEnum):
     RR_SUBAWARD_BUDGET = "RRSubawardBudget"
     RR_SUBAWARD_BUDGET_30 = "RRSubawardBudget30"
     RR_SUBAWARD_BUDGET_10_30 = "RRSubawardBudget10_30"
+    RR_SF424 = "RRSF424"
     SF424_SHORT = "SF424Short"
     SF424A = "SF424A"
     SF424B = "SF424B"
